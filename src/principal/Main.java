945531db3d49aa6681a003d4a3501297cc0089a6
David Perea <david.perea@iesnervion.es>
2022-01-09 21:39:40 +0100

    Initial commit

---
+++ src/principal/Main.java
@@ -0,0 +1,121 @@
+package principal;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+
+        libro libro1 =new libro(111111, "El primer libro ","Fernando ",96);
+
+        libro libro2 =new libro(222222, "El segundo libro ","David ",64);
+
+
+        System.out.println(libro1.toString());
+        System.out.println(libro2.toString());
+
+
+
+        if(libro1.getNumPaginas() > libro2.getNumPaginas()){
+
+            System.out.println(libro1.getTitulo() +" tiene más páginas");
+        }else{
+            System.out.println(libro2.getTitulo() + " tiene más páginas");
+        }
+
+
+
+
+
+        /* ARRAY
+
+        int [] miArray = new int[9];
+
+        for(int i = 0; i< miArray.length;i++){
+
+            miArray[i]= (int) (Math.random()*100);
+
+        }
+
+        for(int i=0; i< miArray.length;i++){
+
+            System.out.println("El array " + i +" "+ miArray[i]);
+
+        }
+
+
+
+         */
+
+    }
+
+
+
+
+
+
+
+
+    public static class libro {
+
+        /*Atributos*/
+        private int ISBN;
+        private String titulo;
+        private String autor;
+        private int numPaginas;
+
+
+
+        /*Constructores*/
+        public libro(int pISBN, String pTitulo, String pAutor, int pNumPaginas){
+
+            ISBN=pISBN;
+            titulo=pTitulo;
+            autor=pAutor;
+            numPaginas=pNumPaginas;
+
+        }
+
+        /*Metodos*/
+
+        public int getISBN() {
+            return ISBN;
+        }
+
+        public void setISBN(int ISBN) {
+            this.ISBN = ISBN;
+        }
+
+        public String getTitulo() {
+            return titulo;
+        }
+
+        public void setTitulo(String titulo) {
+            this.titulo = titulo;
+        }
+
+        public String getAutor() {
+            return autor;
+        }
+
+        public void setAutor(String autor) {
+            this.autor = autor;
+        }
+
+        public int getNumPaginas() {
+            return numPaginas;
+        }
+
+        public void setNumPaginas(int numPaginas) {
+            this.numPaginas = numPaginas;
+        }
+
+
+        public String toString(){
+
+            return "El libro " +titulo+" con ISBN "+ISBN+ " creado por el autor " +autor+"tiene " +numPaginas+" páginas";
+        }
+
+
+
+    }
+}
